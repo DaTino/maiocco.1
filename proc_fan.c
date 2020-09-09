4f86c19d2faf52a70abfc46e1852debe925a33e7
DaTino <36283321+DaTino@users.noreply.github.com>
2020-09-08 20:03:43 -0500

    fixed missing ; on 32

--- proc_fan.c
+++ proc_fan.c
@@ -29,7 +29,7 @@ int main(int argc, char *argv[]) {
         else {
           fprintf (stderr, "Unkown option character `\\x%x`.\n", optopt);
         }
-        return 1
+        return 1;
       default:
         abort();
     }
